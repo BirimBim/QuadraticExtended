5c213ba3d1a1fcc033e9632ae910750ca0d18cd4
BirimBim <125955981+BirimBim@users.noreply.github.com>
2024-09-22 17:36:52 -0700

    Project completed

--- Quadratic/Resource.cpp
+++ Quadratic/Resource.cpp
@@ -2,12 +2,20 @@
 #include <cmath>
 
 using namespace std;
-double delta;
-double Fdelta;
-double quadratic(double a, double b, double c){
-        Fdelta = (((b*b)-4)*a)*c;
-        if(Fdelta<0){
-         delta = sqrt(Fdelta);
-        }
-    return delta;
+
+void restart(bool *running){
+    int choice;
+    cout << "\n\nPress:\n1.To restart: \n2.To quit: \n";
+    cin >> choice;
+    if(choice==2){
+        *running = false;
+    }
 }
+
+void quadratic(double a, double b, double c, double *delta, double *root1, double *root2){
+            *delta = (b*b)+(-4*a*c);
+            *root1 = (-b+sqrt(*delta))/(2*a);
+            *root2 = (-b-sqrt(*delta))/(2*a);
+}
+ 
+ 
--- Quadratic/Source.cpp
+++ Quadratic/Source.cpp
@@ -4,8 +4,33 @@
 
 using namespace std;
 
+bool running = true;
+
 int main()
 {
-    double cu = quadratic(8,2,3);
-    cout << "\npenis " << cu;
+    
+    while(running == true){
+        double a, b, c, delta, root1, root2;
+
+        //Introduction
+        cout << "\nThis program will calculate a quadratic equation (ax^2±bx±c = 0)\nUsing the quadratic formula(x = (-b ± √Δ)÷2a)(Δ=b^-4ac).";
+        cout << "\nKeep in mind that: If a = 0 you do not have a quadratic equation.\n\nInsert a number for a: ";
+        cin >> a;
+        cout << "Insert a number for b: ";
+        cin >> b;
+        cout << "Insert a number for c: ";
+        cin >> c;
+
+        quadratic(a,b,c,&delta,  &root1, &root2);
+
+        if(delta >= 0 && a != 0){
+            cout << "\nRoot 1: " << root1 << "\nRoot 2: " << root2;
+        }else if(delta < 0 && a != 0){
+            cout << "\nError: Δ is negative in this equation making it impossible to square root. Given this equation has no real solution: " << b << "^2-4x" << a << "x" << c << " = " << delta;
+        }else{
+            cout << "\nError: a cannot be = 0";
+        }
+        
+        restart(&running);
+    }
 }
