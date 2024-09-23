a3f75c384c33e22baa95d5c4460338d9ba3f8917
BirimBim <125955981+BirimBim@users.noreply.github.com>
2024-09-22 21:35:46 -0700

    clarified coeficient (a)

--- Quadratic/Source.cpp
+++ Quadratic/Source.cpp
@@ -14,11 +14,11 @@ int main()
 
         //Introduction
         cout << "\nThis program will calculate a quadratic equation (ax^2±bx±c = 0)\nUsing the quadratic formula(x = (-b ± √Δ)÷2a)(Δ=b^-4ac).";
-        cout << "\nKeep in mind that: If a = 0 you do not have a quadratic equation.\n\nInsert a number for a: ";
+        cout << "\nKeep in mind that: If a = 0 you do not have a quadratic equation.\n\nInsert a number for the coeficient a: ";
         cin >> a;
-        cout << "Insert a number for b: ";
+        cout << "Insert a number for the coeficient b: ";
         cin >> b;
-        cout << "Insert a number for c: ";
+        cout << "Insert a number for the coeficient c: ";
         cin >> c;
 
         quadratic(a,b,c,&delta,  &root1, &root2);
@@ -28,7 +28,7 @@ int main()
         }else if(delta < 0 && a != 0){
             cout << "\nError: Δ is negative in this equation making it impossible to square root. Given this equation has no real solution: " << b << "^2-4x" << a << "x" << c << " = " << delta;
         }else{
-            cout << "\nError: a cannot be = 0";
+            cout << "\nError: the coeficient \"a\" cannot be = 0";
         }
         
         restart(&running);
