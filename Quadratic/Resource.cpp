ce30b808c68609075b241be17dd3180f39bcfe94
BirimBim <125955981+BirimBim@users.noreply.github.com>
2024-09-26 22:13:09 -0700

    Imaginary roots added

--- Quadratic/Resource.cpp
+++ Quadratic/Resource.cpp
@@ -12,10 +12,18 @@ void restart(bool *running){
     }
 }
 
-void quadratic(double a, double b, double c, double *delta, double *root1, double *root2){
-            *delta = (b*b)+(-4*a*c);
+void quadratic(double a, double b, double c, double *delta, double *root1, double *root2, bool *complexAnswer){
+            *delta = (b*b)+(-4*(a*c));
+            if(*delta<0){
+                *complexAnswer=true;
+                *delta = *delta*(-1);
+                *delta = sqrt(*delta)/2*a;
+                *root1 = (-b)/(2*a);
+            }else{
             *root1 = (-b+sqrt(*delta))/(2*a);
             *root2 = (-b-sqrt(*delta))/(2*a);
+            }
 }
+//\nError: Δ is negative in this equation making it impossible to square root. Given this equation has no real solution: 
  
  
--- Quadratic/Source.cpp
+++ Quadratic/Source.cpp
@@ -11,6 +11,7 @@ int main()
     
     while(running == true){
         double a, b, c, delta, root1, root2;
+        bool complexAnswer=false;
 
         //Introduction
         cout << "\nThis program will calculate a quadratic equation (ax^2±bx±c = 0)\nUsing the quadratic formula(x = (-b ± √Δ)÷2a)(Δ=b^-4ac).";
@@ -21,12 +22,12 @@ int main()
         cout << "Insert a number for the coeficient c: ";
         cin >> c;
 
-        quadratic(a,b,c,&delta,  &root1, &root2);
+        quadratic(a,b,c,&delta,  &root1, &root2, &complexAnswer);
 
-        if(delta >= 0 && a != 0){
+        if(complexAnswer==false && a != 0){
             cout << "\nRoot 1: " << root1 << "\nRoot 2: " << root2;
-        }else if(delta < 0 && a != 0){
-            cout << "\nError: Δ is negative in this equation making it impossible to square root. Given this equation has no real solution: " << b << "^2-4x" << a << "x" << c << " = " << delta;
+        }else if(complexAnswer==true && a != 0){
+            cout << "\nImaginary roots = " << root1 << " ± " << delta << "i\n";
         }else{
             cout << "\nError: the coeficient \"a\" cannot be = 0";
         }
